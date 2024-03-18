733724a3755f283d42f65bbc11d25b2de455568e
James Shapiro <jamesshapiro@gmail.com>
2024-03-18 14:53:21 -0400

    first commit

---
+++ src/templates/js.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as style from './COMPONENT_NAME.module.css'
+
+function COMPONENT_NAME() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default COMPONENT_NAME;
